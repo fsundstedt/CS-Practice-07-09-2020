4898859228299d1e8d8079edb179623630023672
unknown <frank.sundstedt@gmail.com>
2020-07-09 14:52:47 -0400

    first commit. basic practice app functional

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice_07_09_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Frank", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Item");
+            Console.WriteLine(account.Balance);
+
+            account.MakeDeposit(300, DateTime.Now, "Deposit 1");
+
+            Console.WriteLine(account.GetAccountHistory());
+
+        }
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Practice_07_09_2020
+{
+    class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public String Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            Amount = amount;
+            Date = date;
+            Notes = note;
+        }
+    }
+}
